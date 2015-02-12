1f4a538ef744bab8bea5396be0eabb3528bcb6c1
marinusjvv <marinus.jvv1@gmail.com>
2015-02-12 20:21:02 +0200

    fixed db connection

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -1,31 +1,16 @@
 <?php
-
 return array(
-    'default' => 'sqlite',
-    'connections' => array(
-        'sqlite' => array(
-            'driver'   => 'sqlite',
-            'database' => ':memory:',
-            'prefix'   => ''
-        ),
-    )
-);
-
-// attempt to use real data
-// sqllite db in memory does not allow enum, so unable to use artisan migrate
-// the setup below returns an error: Database does not exist, even though it does exist
-return array(
-    'default' => 'sqlite',
+    'default' => 'mysql',
     'connections' => array(
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => 'localhost',
             'database'  => 'practice_test',
             'username'  => 'root',
-            'password'  => '',
+            'password'  => 'pass',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
         ),
-    )
+    ),
 );
