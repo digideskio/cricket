7880f60c10d732034c6794f0e9109fd2eeb9e595
marinusjvv <marinus.jvv1@gmail.com>
2015-03-09 14:56:18 +0200

    Fixed foreign key issue

--- app/database/migrations/2015_02_07_125241_create_event_vendor.php
+++ app/database/migrations/2015_02_07_125241_create_event_vendor.php
@@ -14,9 +14,9 @@ class CreateEventVendor extends Migration {
 		Schema::create('event_vendor', function($table)
 		{
 			$table->increments('id');
-			$table->integer('event_id');
+			$table->unsignedInteger('event_id');
 			$table->foreign('event_id')->references('id')->on('events');
-			$table->integer('vendor_id');
+			$table->unsignedInteger('vendor_id');
 			$table->foreign('vendor_id')->references('id')->on('vendors');
 			$table->enum('active', array('yes', 'no'))->default('yes');
 			$table->unique('event_id', 'vendor_id');
--- app/database/migrations/2015_02_26_074223_create_vendor_item.php
+++ app/database/migrations/2015_02_26_074223_create_vendor_item.php
@@ -14,11 +14,11 @@ class CreateVendorItem extends Migration {
 		Schema::create('vendor_item', function($table)
 		{
 			$table->increments('id');
-			$table->integer('vendor_id')->unsigned();
+			$table->unsignedInteger('vendor_id');
 			$table->foreign('vendor_id')->references('id')->on('vendors');
-			$table->integer('item_id');
+			$table->unsignedInteger('item_id');
 			$table->foreign('item_id')->references('id')->on('items');
-			$table->integer('event_id');
+			$table->unsignedInteger('event_id');
 			$table->foreign('event_id')->references('id')->on('events');
 			$table->enum('active', array('yes', 'no'))->default('yes');
 			$table->timestamps();
